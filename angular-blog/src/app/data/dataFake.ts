304c908f90b23a8c07b6e0b95d3d12133957beae
monica.marcal <monicagmarcal@outlook.com>
2023-09-28 16:45:20 -0300

    feat: deixando conteudos mais dinamicos

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -2,14 +2,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "Premio framework",
-    "description": "angular recebe premio de melhor framework confira",
-    "photoCover":"aaa"
+    "title": "NOVO ANGULAR",
+    "description": "Google anuncia novo Angular",
+    "photoCover":"https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg"
   },
   {
     "id":"2",
-    "title": "Novo Angular anunciado pelo google +",
+    "title": "Como usar angular CLI",
     "description": "bla blabla",
-    "photoCover":"bbb"
+    "photoCover":"https://www.fabricadecodigo.com/wp-content/uploads/Primeiros-passos-com-o-angular-cli.png"
+  },
+  {
+    "id":"3",
+    "title": "Como usar angular CLI",
+    "description": "bla blabla",
+    "photoCover":"https://www.fabricadecodigo.com/wp-content/uploads/Primeiros-passos-com-o-angular-cli.png"
+  },
+  {
+    "id":"4",
+    "title": "Como usar angular CLI",
+    "description": "bla blabla",
+    "photoCover":"https://www.fabricadecodigo.com/wp-content/uploads/Primeiros-passos-com-o-angular-cli.png"
   }
 ]
